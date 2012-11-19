de4b46ed7bb37d38f9f22550aff98a24fef1324e
Jeroen Bourgois <jeroenbourgois@gmail.com>
2012-11-19 23:29:05 +0100

    updated tmux

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -11,16 +11,14 @@ function tt() {
   echo $sn
 
   # This will also be the default cwd for new windows created
-  tmux new-session -d -s "$sn" 'reattach-to-user-namespace -l bash vim .'
-# 
-#   # New window
-#   tmux new-window -t "$sn:1" 'reattach-to-user-namespace -l bash'
-#   tmux new-window -t "$sn:2" 'reattach-to-user-namespace -l bash'
-#   tmux new-window -t "$sn:3" 'reattach-to-user-namespace -l bash'
-# 
-#   # Select window #1 and attach to the session
-#   tmux select-window -t "$sn:1"
-#   tmux -2 attach-session -t "$sn"
+  tmux new-session -d -s "$sn" 'reattach-to-user-namespace -l zsh'
+
+  # New window
+  tmux new-window -t "$sn:2" 'reattach-to-user-namespace -l mutt'
+
+  # Select window #1 and attach to the session
+  tmux select-window -t "$sn:1"
+  tmux -2 attach-session -t "$sn"
 }
 
 # for tmux: export 256color
