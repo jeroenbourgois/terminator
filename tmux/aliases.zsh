1767cb8082027df2d834a9f75829ef5dc6fc471d
Jeroen Bourgois <jeroenbourgois@gmail.com>
2012-11-19 23:15:53 +0100

    fix tmux bin and config

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -1,22 +1,26 @@
-alias t="tmux -u"
-alias tkill="tmux kill-server"
-
-alias t-a='t attach-session -t $@'
+alias t='tmux'
+alias tk='tmux kill-server'
+alias ta='t attach-session -t $@'
+alias tl='t ls'
 
 function tt() {
   # var for session name (to avoid repeated occurences)
   # sn=$RANDOM
   sn=`echo ${PWD##*/}`
 
+  echo $sn
+
   # This will also be the default cwd for new windows created
-  t new-session -d -s "$sn" 'reattach-to-user-namespace -l bash vim .'
-
-  # New window
-  t new-window -t "$sn:2" 'reattach-to-user-namespace -l bash'
-
-  # Select window #1 and attach to the session
-  t select-window -t "$sn:1"
-  t -2 attach-session -t "$sn"
+  tmux new-session -d -s "$sn" 'reattach-to-user-namespace -l bash vim .'
+# 
+#   # New window
+#   tmux new-window -t "$sn:1" 'reattach-to-user-namespace -l bash'
+#   tmux new-window -t "$sn:2" 'reattach-to-user-namespace -l bash'
+#   tmux new-window -t "$sn:3" 'reattach-to-user-namespace -l bash'
+# 
+#   # Select window #1 and attach to the session
+#   tmux select-window -t "$sn:1"
+#   tmux -2 attach-session -t "$sn"
 }
 
 # for tmux: export 256color
