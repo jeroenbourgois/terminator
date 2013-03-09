979c03161428fb5f80d41858f26a0258ebd4f1d1
Jeroen Bourgois <jeroenbourgois@gmail.com>
2013-03-09 16:57:56 +0100

    Update aliases.zsh
    fixed svn aliases

--- svn/aliases.zsh
+++ svn/aliases.zsh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-source $HOME/.terminator/sys/colors.sh
+source $ZSH/custom/LS_COLORS
 
 alias svns='echo -e "${COLOR_BLUE} » svn status ${COLOR_RESET}"; svn st'
 alias svnc='echo -e "${COLOR_YELLOW} » svn commit -m ${COLOR_RESET}"; svn commit -m "$@"'
 alias svnu='echo -e "${COL_BLUE} » svn up ${COLOR_RESET}"; svn up '
 alias svnl='echo -e "${COL_YELLOW} » svn log -l 5 ${COLOR_RESET}"; svn log -l 5'
 
-alias svn-status-all='~/.terminator/svn/bin/svnstatus.py $@'
-alias svn-up-all='~/.terminator/svn/bin/svnup.py $@'
+alias svn-status-all='$ZSH/svn/bin/svnstatus.py $@'
+alias svn-up-all='$ZSH/svn/bin/svnup.py $@'
