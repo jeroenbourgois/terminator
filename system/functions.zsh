b5759031af43114bcc1d59411acc0bac44cd7c34
Jeroen Bourgois <jeroenbourgois@gmail.com>
2014-06-18 10:47:10 +0200

    local func for git status

--- system/functions.zsh
+++ system/functions.zsh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-# Create a new directory and enter it
-# md() {
-#   mkdir -p "$@" && cd "$@"
-# }
-
 # like Sherlock Holmes, find process
 holmes() {
   ps aux | grep $@ | grep -v grep
+}
+
+git-status-all() {
+  for gitdir in `find . -name .git`;
+  do
+    local workdir=$(dirname $gitdir);
+    local gitout="`git -c color.status=always --git-dir=$gitdir --work-tree=$workdir status`";
+
+    if [[ ! $gitout =~ .*nothing\ to\ commit,\ working\ directory\ clean.* ]];
+    then
+      echo;
+      echo $workdir;
+      echo $gitout;
+      echo "###########################################";
+    fi
+  done
 }
