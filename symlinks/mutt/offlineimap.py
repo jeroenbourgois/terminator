5dd6e50842a05d969cdcba94242409b6a6195a4d
jeroenb <jeroenbourgois@gmail.com>
2016-09-29 12:36:32 +0200

    msmtp

--- symlinks/mutt/offlineimap.py
+++ symlinks/mutt/offlineimap.py
@@ -1,5 +1,17 @@
-#! /usr/bin/env python2
-from subprocess import check_output
+#!/usr/bin/python
+import re, subprocess
+def get_keychain_pass(account=None, server=None):
+    params = {
+        'security': '/usr/bin/security',
+        'command': 'find-internet-password',
+        'account': account,
+        'server': server,
+        'keychain': '~/Library/Keychains/login.keychain',
+    }
+    command = "sudo -u jeroenb %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    outtext = [l for l in output.splitlines()
+               if l.startswith('password: ')][0]
 
-def get_pass():
-    return check_output("gpg -dq ~/.mutt/offlineimappass.gpg", shell=True).strip("\n")
+    return re.match(r'password: "(.*)"', outtext).group(1)
+
