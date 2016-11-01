c1f9524a0af62c89455e387045e7dd6cdb449d37
jeroenb <jeroenbourgois@gmail.com>
2016-11-01 08:54:31 +0100

    colors and mutt

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 set -g -x PATH /usr/local/bin /usr/local/sbin $PATH
 
 . ~/.config/fish/aliases.fish
+
+# Base16 Shell
+if status --is-interactive
+    eval sh $HOME/.terminator/config/base16-shell/scripts/base16-google-light.sh
+end
